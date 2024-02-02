c49fb1f96fa199563d66bd36d82292be1bd35bbb
joshuahahn <joshua.hahnjy@gmail.com>
2024-02-01 20:31:32 -0500

    final

--- src/data_generator.py
+++ src/data_generator.py
@@ -8,17 +8,17 @@ from numpy import random as nprandom
 import movie
 
 # Global genre list
-GENRES = ["Action", "Romance", "Mystery", "Comedy", "Horror", "Crime", "Thriller"]
+GENRES = ["Action", "Romance", "Mystery", "Comedy", "Horror", "Crime", "Thriller", "Romcom"]
 VEC_LENGTH = len(GENRES)
 
 # Global attribute list
-ATTRIBUTES = ["Amazing", "Awesome", "Boring", "Cool", ]
+ATTRIBUTES = ["Amazing", "Awesome", "Boring", "Cool", "Fun", "Exciting", "Heartwrenching"]
 
 # Global movie name list
-TYPE = ["Movie", "Indie Film", "Film", "Animation", "Live-action Film"]
+TYPE = ["Movie", "Indie Film", "Film", "Animation", "Live-action Film", "Show", "Biopic"]
 
 
-def generate_movie():
+def generate_movie(seen=False):
     """
     Generates a movie object with randomly generated attributes.
     One movie is selected as the 'primary genre' with a normal 
@@ -27,7 +27,11 @@ def generate_movie():
     Every other genre gets some small noise.
     """
 
-    primary_genre = random.randint(0,VEC_LENGTH-1)
+    if seen:
+        primary_genre = 0
+    else:
+        primary_genre = random.randint(0,VEC_LENGTH-1)
+
     secondary_genre = random.randint(0,VEC_LENGTH-1)
     if secondary_genre == primary_genre:
         secondary_genre = (secondary_genre + random.randint(1, VEC_LENGTH-1)) % VEC_LENGTH
@@ -48,10 +52,21 @@ def generate_movie():
 
     return (title, vector)
 
-def generate_dataset(n=100):
+def generate_dataset(n=1000):
     file = open('dataset.txt', 'w')
     for _ in range(n):
         title, vector = generate_movie()
+        curr_line = [title] + vector
+        for i in range(1, len(curr_line)):
+            curr_line[i] = str(curr_line[i])
+        file.write(','.join(curr_line))
+        file.write('\n')
+    file.close()
+
+    file = open('seenmovies.txt', 'w')
+    for _ in range(20):
+        # Let's simulate someone who likes action movies. 
+        title, vector = generate_movie(seen=True)
         curr_line = [title] + vector
         for i in range(1, len(curr_line)):
             curr_line[i] = str(curr_line[i])
--- src/recommender.py
+++ src/recommender.py
@@ -1,3 +1,98 @@
+import movie
+import math
+
 class Recommender:
     """A class that contains the movie dataset and recommendation methods"""
     
+    def __init__(self, dataset_path='./dataset.txt', seen_path='./seenmovies.txt'):
+        """Initializes the recommender class with the dataset path given."""
+        csv = open(dataset_path)
+        self.dataset = []
+        for line in csv:
+            tokens = line.split(",")
+            title = tokens[0]
+            vector = tokens[1:]
+            self.dataset.append(movie.Movie(title, vector))
+        csv.close()
+
+        csv = open(seen_path)
+        self.seen = []
+        for line in csv:
+            tokens = line.split(",")
+            title = tokens[0]
+            vector = tokens[1:]
+            self.seen.append(movie.Movie(title, vector))
+        csv.close()
+
+    def distance(self, movie1, movie2, metric='Euclidean'):
+        if metric == "Euclidean":
+            return self.euclidean(movie1, movie2)
+        elif metric == "Manhattan":
+            return self.manhattan(movie1, movie2)
+
+    def euclidean(self, movie1, movie2):
+        distance = 0
+        n = len(movie1)
+
+        if len(movie2) != n:
+            return -1 # Error
+
+        for i in range(n):
+            distance += (movie1[i] - movie2[i])**2
+        
+        return distance ** 0.5
+
+    def manhattan(self, movie1, movie2):
+        distance = 0
+        n = len(movie1)
+
+        if len(movie2) != n:
+            return -1 # Error
+        
+        for i in range(n):
+            distance += math.abs(movie1[i] - movie2[i])
+        
+        return distance
+    
+    def get_recommendation(self, metric='Euclidean'):
+        minimum_distance = float('inf')
+        recommendation = None
+
+        for M in self.dataset:
+            current_distance = 0
+
+            for L in self.seen:
+                current_distance += self.distance(M.vector, L.vector, metric)
+            
+            if current_distance < minimum_distance:
+                minimum_distance = current_distance
+                recommendation = M.name
+
+        return recommendation
+    
+    def get_many_recommendations(self, k=10, metric='Euclidean'):
+        movie_distances = []
+
+        for M in self.dataset:
+            current_distance = 0
+
+            for L in self.seen:
+                current_distance += self.distance(M.vector, L.vector, metric)
+            
+            movie_distances.append((current_distance, M.name))
+        
+        movie_distances.sort()
+
+        recommendations = []
+        for i in range(k):
+            recommendations.append(movie_distances[i][1])
+        return recommendations
+
+a = Recommender()
+recommendation = a.get_recommendation()
+print("Single recommendation:\n" + recommendation)
+print("===============================")
+recommendations = a.get_many_recommendations()
+print("Mutiple Recomendations: ")
+for recommendation in recommendations:
+    print(recommendation)
